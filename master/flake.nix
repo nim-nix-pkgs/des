931975c68fba3ee27a06fbf91c34e830a3d2b56d
hugosenari <hugosenari@gmail.com>
2022-03-12 02:39:10 -0300

    chore: re index des

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''3DES native library for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-des-master.flake = false;
+  inputs.src-des-master.owner = "LucaWolf";
+  inputs.src-des-master.ref   = "refs/heads/master";
+  inputs.src-des-master.repo  = "des.nim";
+  inputs.src-des-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-des-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-des-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
